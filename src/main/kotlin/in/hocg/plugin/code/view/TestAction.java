cd09f84b0be5e240a962b2f76b7901911fe545b2
hocgin <hocgin@gmail.com>
2019-12-24 23:08:16 +0800

    init

---
+++ src/main/kotlin/in/hocg/plugin/code/view/TestAction.java
@@ -0,0 +1,18 @@
+package in.hocg.plugin.code.view;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by hocgin on 2019/12/24.
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+public class TestAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
