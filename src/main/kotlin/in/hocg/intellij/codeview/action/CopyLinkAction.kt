dfcc2929d91ce226a8f4dcc9e9ee9415e5583116
hocgin <hocgin@gmail.com>
2020-01-02 23:21:33 +0800

    更新

--- src/main/kotlin/in/hocg/intellij/codeview/action/CopyLinkAction.kt
+++ src/main/kotlin/in/hocg/intellij/codeview/action/CopyLinkAction.kt
@@ -1,8 +1,10 @@
 package `in`.hocg.intellij.codeview.action
 
+import com.intellij.ide.CopyPasteManagerEx
 import com.intellij.notification.Notification
 import com.intellij.notification.NotificationAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import java.awt.datatransfer.StringSelection
 
 /**
  * Created by hocgin on 2019/12/27.
@@ -10,12 +12,18 @@ import com.intellij.openapi.actionSystem.AnActionEvent
  * 点击复制链接
  * @author hocgin
  */
-class CopyLinkAction(text: String = "复制链接") : NotificationAction(text) {
+class CopyLinkAction(
+    text: String = "复制链接",
+    private var link: String = ""
+) : NotificationAction(text) {
+
+
     override fun update(e: AnActionEvent) {
     }
 
     override fun actionPerformed(e: AnActionEvent, notification: Notification) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        CopyPasteManagerEx.getInstance()
+            .setContents(StringSelection("""点击访问: $link"""))
     }
 
 }
